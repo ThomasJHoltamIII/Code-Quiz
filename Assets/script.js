a054c44b0bb45ab5af75a6ee6d8c0ec234806dfa
Thomas Holtam <thomasjholtamIII@gmail.com>
2023-11-11 11:49:45 -0700

    Added a basic html and css layout and psuedo coded JS

---
+++ Assets/script.js
@@ -0,0 +1,24 @@
+// Make variable for right answers
+
+// Make variable for wrong answers
+
+// Make a timer
+
+// Bind wrong answers to a vairable that - from timer
+
+// Make score vairable 
+
+// Bind score to local memory
+
+// Make a varible that houses the questions
+
+// bind varible to another that appends a new question and its answers on a click
+
+// Make a enter intials vairble that allows a local memery storing of score+name
+
+// On timer = 0 pull up enter intials and "Lose" vairable
+
+// On score <= question count pull up enter initials
+
+// Make vairable that displays when on the "High Score" page 
+
