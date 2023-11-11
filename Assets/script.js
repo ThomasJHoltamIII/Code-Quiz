43861a35576ae39901ee4aec84076f6bcef25312
Thomas Holtam <thomasjholtamIII@gmail.com>
2023-11-11 13:36:19 -0700

    Added funtion to questions, and a score tracker

--- Assets/script.js
+++ Assets/script.js
@@ -1,62 +1,120 @@
+// Question array
+var quizQuestions = [
+    "What is the correct way to declare a variable in JavaScript?",
+    "Which of the following methods is used to add an element to an array in JavaScript?",
+    "How do you write a comment in JavaScript?",
+    "What does the === operator do in JavaScript?"
+];
 
-
-// Array of right answers
+// right answer array
 var rightAnswers = [
-    "let x;",        // Correct answer for Question 1
-    "To convert a string to an integer.", // Correct answer for Question 2
-    "// This is a comment", // Correct answer for Question 3
-    "Compares two values for equality without type coercion." // Correct answer for Question 4
-  ];
-  
-// Wrong answer array
-var wrongAnswers = [
-    ["var x;", "variable x;", "x = variable;"], // Wrong answers for Question 1
-    ["To print text to the console.", "To create a new array.", "To define a new function."], // Wrong answers for Question 2
-    ["<!-- This is a comment -->", "/* This is a comment */", "* This is a comment *"], // Wrong answers for Question 3
-    ["Assigns a value to a variable.", "Checks if a variable is defined.", "Performs a logical OR operation."] // Wrong answers for Question 4
-  ];
+    "let x;",
+    "push()",
+    "// This is a comment",
+    "Compares two values with strict equality."
+];
 
-//Timer, binds to start button 
+// wrong answer array
+var wrongAnswers = [
+    ["x();", "variable x;", "x = variable;"],
+    ["add()", "insertElement()", "addToEnd()"],
+    ["~This is a comment~", "/* This is a comment */", "* This is a comment *"],
+    ["Assigns a value to a variable.", "Checks if a variable is defined.", "Performs a logical OR operation."]
+];
+
+// Value set for my tracked variables
 let timeLeft = 40;
 let timerInterval;
+let currentQuestions = 0;
+let score = 0;
 
-document.getElementById("start").addEventListener("click", function() {
-  if (timerInterval) {
-    clearInterval(timerInterval);
-  }
-
-  timeLeft = 40;
-  updateTimer();
-
-  timerInterval = setInterval(function() {
-    timeLeft--;
-    updateTimer();
-
-    if (timeLeft === 0) {
-      clearInterval(timerInterval);
-      alert("Time's up!");
-    }
-  }, 1000);
-});
-
+// Timer functions
 function updateTimer() {
-  document.getElementById("timer").innerText = `${timeLeft}`;}
+    document.getElementById("timer").innerText = `${timeLeft} `;
+}
+
+function startTimer() {
+    timerInterval = setInterval(function () {
+        timeLeft--;
+        updateTimer();
+
+        if (timeLeft <= 0) {
+            endQuiz();
+        }
+    }, 1000);
+}
+
+// Function to start the quiz
+function startQuiz() {
+    hideWelcomeContainer();
+    showQuestion();
+    startTimer();
+}
+
+// startQuiz event listener
+document.getElementById("start").addEventListener("click", startQuiz);
+
+
+// Function to hide the container with welcome elements
+function hideWelcomeContainer() {
+    document.querySelector('.welcome-text').style.display = 'none';
+    document.querySelector('.start-button').style.display = 'none';
+}
+
+// Array Shuffle
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+
+// Question Elemtents
+function showQuestion() {
+    let currentQuestion = quizQuestions[currentQuestions];
+    let currentChoices = wrongAnswers[currentQuestions].concat(rightAnswers[currentQuestions]);
+
+    // Shuffle the choices to display in random order
+    currentChoices = shuffleArray(currentChoices);
+
+    // Display question and choices in your HTML
+    const questionContainer = document.getElementById("question-container");
+    questionContainer.innerHTML = '';
+
+    const questionElement = document.createElement("h3");
+    questionElement.textContent = currentQuestion;
+    questionContainer.appendChild(questionElement);
+
+    currentChoices.forEach(choice => {
+        const button = document.createElement("button");
+        button.textContent = choice;
+        button.addEventListener("click", () => checkAnswer(choice));
+        questionContainer.appendChild(button);
+    });
+}
+
+// Right Answer Check
+function checkAnswer(selectedAnswer) {
+    if (selectedAnswer === rightAnswers[currentQuestions]) {
+        score++;
+    }
+
+    currentQuestions++;
+
+    if (currentQuestions < quizQuestions.length) {
+        showQuestion();
+    } else {
+        endQuiz();
+    }
+}
+
+function endQuiz() {
+    clearInterval(timerInterval)
+    console.log(score); }
+
 
 // Bind wrong answers to a vairable that - from timer
-
-// Make score vairable 
-
-// Bind score to local memory
-
-// Make a varible that houses the questions
-
-// bind varible to another that appends a new question and its answers on a click
-
-// Make a enter intials vairble that allows a local memery storing of score+name
-
-// On timer = 0 pull up enter intials and "Lose" vairable
-
-// On score <= question count pull up enter initials
 
 // Make vairable that displays when on the "High Score" page 
 
